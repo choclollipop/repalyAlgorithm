f7f054d8d38744849376addad85e93673e11aaa0
choclollipop <2924733894@qq.com>
2024-01-02 14:03:08 +0800

    [修改] 优化代码，减少判断的使用

--- sort/insertSort.c
+++ sort/insertSort.c
@@ -1,35 +1,28 @@
 /* 插入排序 */
 #include <stdio.h>
 
-#define BUFFER_SIZE    6
+#define BUFFER_SIZE    7
 
 int main()
 {
-    int buffer[BUFFER_SIZE] = {24, 30, 5, 58, 12, 39};
+    int buffer[BUFFER_SIZE] = {24, 30, 5, 58, 12, 39, 35};
     /* 取最后一次移动的值的下标即插入位置 */
     int sub = 0;
     /* 取寻找到的应该重新排序的较小值 */
     int copy = 0;
 
     /* 外层循环需找出需要进行排序的数的下标 */
-    for(int idx = 0; idx < BUFFER_SIZE; idx++)
+    for(int idx = 1; idx < BUFFER_SIZE; idx++)
     {
-        if(buffer[idx] > buffer[idx + 1])
+        sub = idx;
+        copy = buffer[idx];
+        /* 后面的值小于前面，则交换 */
+        while (sub > 0 && copy < buffer[sub - 1])
         {
-            copy = buffer[idx + 1];
-            /* 此时的 [idx + 1] 的数据就是小与前面数据的位置 */
-            for(int pos = idx + 1; pos >= 0; pos--)
-            {
-                if(copy < buffer[pos - 1])
-                {
-                    buffer[pos] = buffer[pos - 1];
-                    /* 插入位置 */
-                    sub = pos - 1;
-                }
-            }
-            /* 将此时寻找到的小的数据插入到后移之后的插入位置 */
-            buffer[sub] = copy;
+            buffer[sub] = buffer[sub - 1];
+            sub--;
         }
+        buffer[sub] = copy;
     }
 
     /* 打印数组 */
